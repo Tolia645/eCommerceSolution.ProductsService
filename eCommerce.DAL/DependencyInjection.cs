9a5eb62fa983d533c02d17fa7c0f0f0eded81eb6
ToliaTokarskyi <tolikterrarit@gmail.com>
2025-05-05 15:35:46 +0300

    Add Env variables for mysql db

--- eCommerce.DAL/DependencyInjection.cs
+++ eCommerce.DAL/DependencyInjection.cs
@@ -11,11 +11,15 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccesLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+        string connectionString = connectionStringTemplate
+            .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+            .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
+        
         //add DbContext
         services.AddDbContext<ApplicationDbContext>(options =>
         {
-            options.UseMySql(
-                configuration.GetConnectionString("DefaultConnection")!,
+            options.UseMySql(connectionString,
                 ServerVersion.AutoDetect(configuration.GetConnectionString("DefaultConnection")!)
             );
         });
